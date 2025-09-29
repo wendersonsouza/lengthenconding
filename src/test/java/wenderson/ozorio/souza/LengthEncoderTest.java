a80e8ed33273d6af029e875dca8b88c2ee9c9898
Wenderson de Souza <wsouza@mifinity.com>
2025-09-29 08:28:12 +0100

    adding unit test

--- src/test/java/wenderson/ozorio/souza/LengthEncoderTest.java
+++ src/test/java/wenderson/ozorio/souza/LengthEncoderTest.java
@@ -1,7 +1,21 @@
 package wenderson.ozorio.souza;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Length Encoder Test")
 class LengthEncoderTest {
+
+    @Test
+    @DisplayName("An encoded text will be successfully returned")
+    void givenText_returnEncondedText() throws Exception{
+        String input = "aaaabbbccc";
+        Assertions.assertEquals(LengthEncoder.encode(input), "a4b3c3"); ;
+    }
+
 
 }
